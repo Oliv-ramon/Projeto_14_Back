daa4b5bb90403fcc46234715767d5939db87c9e1
Tarcísio Teles <tfst@ecomp.poli.br>
2022-02-13 11:02:28 -0300

    patch: db query to filter products by category

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,9 +1,17 @@
 import db from "../db.js";
 
 export async function getProducts(req, res) {
-  try {
-    const allProducts = await db.collection("products").find({}).toArray();
-    res.status(200).send(allProducts);
+  try {  
+    let productsQuery = [];
+    const categoryFilter = req.query.cat;
+    console.log(categoryFilter);
+
+    if (!categoryFilter) {
+      productsQuery = await db.collection("products").find({}).toArray();
+    }
+    productsQuery = await db.collection("products").find({category: categoryFilter}).toArray();
+    
+    res.status(200).send(productsQuery);
     return;
   } catch (error) {
     console.log(error);
