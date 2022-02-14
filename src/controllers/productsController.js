bcd90b2aa48e264098c54e4bb930087ccbfd6d82
Tarcísio Teles <tfst@ecomp.poli.br>
2022-02-14 15:57:14 -0300

    feat: 'getProducts', remote database

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,17 +2,10 @@ import db from "../db.js";
 
 export async function getProducts(req, res) {
   try {  
-    let productsQuery = [];
-    const categoryFilter = req.query.cat;
-    console.log(categoryFilter);
-
-    if (!categoryFilter) {
-      productsQuery = await db.collection("products").find({}).toArray();
-    }
-    productsQuery = await db.collection("products").find({category: categoryFilter}).toArray();
-    
-    res.status(200).send(productsQuery);
+    const allProducts = await db.collection("products").find({}).toArray();
+    res.status(200).send(allProducts);
     return;
+    
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
