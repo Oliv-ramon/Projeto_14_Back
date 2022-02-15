20e78a6096133f62a583dd9fd155a5a288257191
ramon oliveira <ramon30012000@gmail.com>
2022-02-15 16:21:31 -0300

    fixing .replace on undefined bug

--- src/middlewares/tokenValidation.js
+++ src/middlewares/tokenValidation.js
@@ -2,8 +2,8 @@ import db from "../db.js";
 
 export default async function tokenValidation(req, res, next) {
   const authorization = req.headers.authorization;
-  console.log(authorization)
-  const token = authorization.replace("Bearer ", "");
+
+  const token = authorization?.replace("Bearer ", "");
   
   if (!token) {
     return res.sendStatus(401);
